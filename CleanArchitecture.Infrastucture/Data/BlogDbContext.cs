bb46bc258e899b6c8d181ed831748fd78f93ae9a
laludonisetiawan <laludonisetiawan@gmail.com>
2025-02-08 18:45:48 +0700

    menambahkan data dbcontext dan repository pada layer infrastucture, dan membuat file interface dan class baru pada direktori service di layer Application

---
+++ CleanArchitecture.Infrastucture/Data/BlogDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchitecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastucture.Data
+{
+    public class BlogDbContext: DbContext
+
+    {
+        // gunakan snippet ctor untuk membuat constuctor
+        public BlogDbContext(DbContextOptions<BlogDbContext> dbContextOptions): base(dbContextOptions) { }
+        
+        public DbSet<Blog> Blogs { get; set; }
+
+    }
+}
