2497ba2cf7e33bf4b8ebf1534b09c703e717768a
laludonisetiawan <laludonisetiawan@gmail.com>
2025-02-10 16:27:52 +0700

    finsih

---
+++ CleanArchitecture.WebAPI/Controllers/BlogController.cs
@@ -0,0 +1,71 @@
+﻿using CleanArchitecture.Application.Services;
+using CleanArchitecture.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        // constructor & dependency injection
+        private readonly IBlogService _blogService;
+        public BlogController(IBlogService blogService) 
+        {
+            _blogService = blogService;
+        }
+
+        // ambil semua data blog
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var blogs = await _blogService.GetAllAsync();
+            return Ok(blogs);
+        }
+
+        // ambil data berdasarkan Id
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var blog = await _blogService.GetByIdAsync(id);
+            if (blog == null)
+            {
+                return NotFound();
+            }
+            return Ok(blog);
+        }
+
+        // membuat data baru
+        [HttpPost]
+        public async Task<IActionResult> Create(Blog blog)
+        {
+            var createdBlog = await _blogService.CreateAync(blog);
+            return CreatedAtAction(nameof(GetById), new { id = createdBlog.Id }, createdBlog);  
+        }
+
+        // mengedit data blog berdasarkan Id
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Blog updateBlog)
+        {
+            int existingBlog = await _blogService.UpdateAsync(id, updateBlog);
+            if (existingBlog == 0)
+            {
+                return BadRequest();
+            }
+            return NoContent();
+        }
+
+        // menghapus data blog berdasarkan Id
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            int blog = await _blogService.DeleteAsync(id);
+            if(blog == 0)
+            {
+                return BadRequest();
+            }
+            return NoContent(); 
+        }
+    }
+}
--- CleanArchitecture.WebAPI/Program.cs
+++ CleanArchitecture.WebAPI/Program.cs
@@ -1,6 +1,18 @@
+using CleanArchitecture.Application.Services;
+using CleanArchitecture.Domain.Interface;
+using CleanArchitecture.Infrastucture.Data;
+using CleanArchitecture.Infrastucture.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<BlogDbContext>(options =>
+options.UseSqlite(builder.Configuration.GetConnectionString("BlogDbContext") ??
+throw new InvalidOperationException("Connection string 'BlogDbContext' not found")));
+
+builder.Services.AddTransient<IBlogRepository, BlogRepository>();
+builder.Services.AddTransient<IBlogService, BlogService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
